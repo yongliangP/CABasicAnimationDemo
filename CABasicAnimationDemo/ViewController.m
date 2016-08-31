62c06806916ea90e8ee603a6b87dc1493e9b304d
yongliangPeng <1994577317@qq.com>
2016-08-31 18:11:16 +0800

    加入了 陀螺仪&加速器 制作动画

--- CABasicAnimationDemo/ViewController.m
+++ CABasicAnimationDemo/ViewController.m
@@ -7,10 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <CoreMotion/CoreMotion.h>
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *sharkTagButton;
 @property (weak, nonatomic) IBOutlet UIButton *alphaTagButton;
+
+//@property (weak, nonatomic) IBOutlet UIButton *alphaTagButton;
+
+@property (nonatomic,strong) CMMotionManager *motionManager;
 
 @end
 
@@ -24,11 +29,57 @@
     [self setUpAlphaAnimation];
     
     //摇摆动画
-    [self setUpSharkAnimation];
+    //[self setUpSharkAnimation];
+    
+    //陀螺仪&加速器
+    [self setUpdeviceMotionAnimation];
     
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+
+-(void)setUpdeviceMotionAnimation
+{
+    self.motionManager = [[CMMotionManager alloc] init];
+    self.motionManager.deviceMotionUpdateInterval = 1.0f/100.0f; //1秒100次
+    self.sharkTagButton.layer.anchorPoint = CGPointMake(0.5, 0);
+    
+    [self.motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMDeviceMotion *motion, NSError *error) {
+        
+        
+        if(fabs(motion.attitude.roll)<1.0f)
+        {
+            [UIView animateWithDuration:0.6 animations:^{
+                self.sharkTagButton.layer.transform = CATransform3DMakeRotation(-(motion.attitude.roll), 0, 0, 1);
+            }];
+        }else if (fabs(motion.attitude.roll)<1.5f)
+        {
+            [UIView animateWithDuration:0.6 animations:^{
+                
+                int s;
+                if (motion.attitude.roll>0)
+                {
+                    s=-1;
+                }else
+                {
+                    s = 1;
+                }
+                self.sharkTagButton.layer.transform = CATransform3DMakeRotation(s*M_PI_2, 0, 0, 1);
+            }];
+            
+        }
+        
+        if ((motion.attitude.pitch)<0)
+        {
+            [UIView animateWithDuration:0.6 animations:^{
+                self.sharkTagButton.layer.transform = CATransform3DMakeRotation(M_PI, 0, 0, 1);
+            }];
+        }
+        
+    }];
+
+
+}
 
 
 -(void)setUpAlphaAnimation
@@ -42,7 +93,7 @@
     animation.removedOnCompletion = NO;
     animation.fillMode = kCAFillModeForwards;//removedOnCompletion,fillMode配合使用保持动画完成效果
     animation.timingFunction=[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
-    [self.alphaTagButton.layer addAnimation:animation forKey:@"aAlpha"];
+    [self.alphaTagButton.layer addAnimation:animation forKey:@"alpha"];
 }
 
 
